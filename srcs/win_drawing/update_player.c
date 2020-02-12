9afad6a2dd08d3fb8a5c681eccf118fd5e367c5e
Martijn Van ginkel <mvan-gin@f1r1s14.codam.nl>
2020-02-12 17:18:38 +0100

    Made view-range rays shoot from right to left

--- srcs/win_drawing/update_player.c
+++ srcs/win_drawing/update_player.c
@@ -6,7 +6,7 @@
 /*   By: mvan-gin <mvan-gin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/05 15:01:59 by mvan-gin       #+#    #+#                */
-/*   Updated: 2020/02/12 14:46:59 by mvan-gin      ########   odam.nl         */
+/*   Updated: 2020/02/12 17:16:05 by mvan-gin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,20 +111,31 @@ static float calculate_ray_distance(t_game_manager *game_manager, t_ray_info *ra
         }
     }
 
-    float perp_wall_dist;
-
     if (side == 0)
     {
-        perp_wall_dist = fabs((map_x_position - game_manager->player_tile->x + (1 - step_x) / 2) / ray_info->ray_x_dir);
-        //printf("x: %f\n", perp_wall_dist);
+        //printf("ray_lengthx : %f\n", ray_info->side_dist_x);
+
+        //printf(":%f\n", cos(M_PI) * game_manager->player_dir);
+
+        // perp_wall_dist = (ray_info->ray_dir - game_manager->player_dir * ray_info->side_dist_x);
+        // // ray_info->side_dist_x * ray
+        // printf("n-perp: %f\n", ray_info->side_dist_x);
+        // printf("perp: %f\n", perp_wall_dist);
+
         return (ray_info->side_dist_x);
     }
     else
     {
-        perp_wall_dist = fabs((map_y_position - game_manager->player_tile->y + (1 - step_y) / 2) / ray_info->ray_y_dir);
-        //printf("y: %f\n", perp_wall_dist);
+        // perp_wall_dist = (game_manager->player_dir - ray_info->ray_dir * ray_info->side_dist_y);
+        // // ray_info->side_dist_x * ray
+        // printf("n-perp: %f\n", ray_info->side_dist_y);
+        // printf("perp: %f\n", perp_wall_dist);
+
+        printf("ray_lengthy : %f\n", ray_info->side_dist_y);
+
+
         return (ray_info->side_dist_y);
-    }  
+    }
 }
 
 
@@ -137,10 +148,14 @@ static float calculate_ray(t_game_manager *game_manager, float ray_dir)
     ray_info.ray_x_dir = sin(ray_dir);
     ray_info.ray_y_dir = cos(ray_dir);
 
+    // printf("%f", )
+
     calculate_side_distances(game_manager, &ray_info);
     calculate_deltas(game_manager, &ray_info);
     // length = calculate_ray_distance
     ray_distance = calculate_ray_distance(game_manager, &ray_info);
+
+    printf("ray_d: %f\n", ray_distance);
 
     return (ray_distance);
     // length wordt naar een draw functie gestuurd om de hoogte te berekenen
@@ -163,25 +178,13 @@ static void clean_wall_line(t_game_manager *game_manager, int x_value)
 static void draw_wall_line(t_game_manager *game_manager, int res_i, float ray_distance)
 {
     int res_height;
-
-    // float percentage;
     int incre;
     int middle;
     
 
     res_height = (int)(game_manager->file_data->resolution[0][1]);
-    // percentage = 1 / ray_distance;
     incre = (((1 / ray_distance) * res_height) * 10);
-
-    // printf("incre:%d\n", incre);
-
-
     middle = (res_height / 2) + (incre / 2);
-
-    printf("ray:%f m:%d in:%d\n", ray_distance, middle, incre);
-
-    // my_mlx_pixel_put(game_manager->img_data2, res_i, middle, 0xFF0000);
-
     while (incre > 0)
     {
         if (middle < 0 || middle > res_height)
@@ -194,7 +197,7 @@ static void draw_wall_line(t_game_manager *game_manager, int res_i, float ray_di
 
 static void shoot_rays(t_game_manager *game_manager, float player_dir, int color)
 {
-    // float   current_ray;
+    float   current_ray;
     float   left_ray;
     float   right_ray;
     float   increment;
@@ -202,24 +205,32 @@ static void shoot_rays(t_game_manager *game_manager, float player_dir, int color
 
 
     i = 0;
-    left_ray = player_dir - ((M_PI / 3) / 2);
-    right_ray = player_dir + ((M_PI / 3) / 2);
+    left_ray = (player_dir - ((M_PI / 3) / 2));
+    right_ray = (player_dir + ((M_PI / 3) / 2));
     increment = (right_ray - left_ray) / (game_manager->file_data->resolution[0][0]);
+    current_ray = left_ray;
 
-    while (left_ray < right_ray)
+    while (current_ray < right_ray)
     {
+        draw_vision_line(game_manager, current_ray, color);
         clean_wall_line(game_manager, i);
-        draw_wall_line(game_manager, i, calculate_ray(game_manager, left_ray));
-        draw_vision_line(game_manager, left_ray, color);
-        left_ray += increment;
+        draw_wall_line(game_manager, i, calculate_ray(game_manager, current_ray));
+        if (i == 20)
+            break;
+        current_ray += increment;
         i++;
     }
-    // draw_wall_line(game_manager, i, calculate_ray(game_manager, game_manager->player_dir));
-    // draw_vision_line(game_manager, game_manager->player_dir, color);
+
+    // float   ray_length;
+    // ray_length = calculate_ray(game_manager, player_dir);
+
+    // printf("ray_length: %f\n", ray_length);
+
+    // draw_vision_line(game_manager, left_ray, color);
+    // draw_wall_line(game_manager, i, ray_length);
+
     mlx_put_image_to_window(game_manager->img_data->mlx, game_manager->img_data->mlx_win, game_manager->img_data->img, 0, 0);
     mlx_put_image_to_window(game_manager->img_data2->mlx, game_manager->img_data2->mlx_win, game_manager->img_data2->img, 0, 0);
-
-
 }
 
 void rotate_player(t_game_manager *game_manager, double rotation)
@@ -245,8 +256,8 @@ void move_player(t_game_manager *game_manager, double walk_speed)
     if (available_pixel(game_manager, new_x_value, new_y_value))
     {
         game_manager->player_tile = &game_manager->map[(int)new_y_value / game_manager->tile_height][(int)new_x_value / game_manager->tile_width];
-        printf("%f ", new_x_value);
-        printf("%f\n", new_y_value);
+        // printf("%f ", new_x_value);
+        // printf("%f\n", new_y_value);
         draw_vision_line(game_manager, game_manager->player_dir, 0xFF0000);
         game_manager->player_x = new_x_value;
         game_manager->player_y = new_y_value;
